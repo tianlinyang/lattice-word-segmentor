78cca6b055b377bd9afccc03101d3ccf0d4453fb
WenjingZhang0214 <35064524+WenjingZhang0214@users.noreply.github.com>
2018-06-21 13:50:26 +0800

    Add files via upload

--- src/train_w.py
+++ src/train_w.py
@@ -2,10 +2,10 @@
 from __future__ import print_function
 import time
 import codecs
-from src.model.crf import *
-from src.model.lstm_crf import *
-import src.model.utils as utils
-from src.model.evaluator import eval_w
+from model.crf import *
+from model.lstm_crf import *
+import model.utils as utils
+from model.evaluator import eval_w
 
 import argparse
 import numpy as np
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     parser.add_argument('--patience', type=int, default=20, help='patience for early stop')
     parser.add_argument('--least_iters', type=int, default=50, help='at least train how many epochs before stop')
     parser.add_argument('--shrink_embedding', type=bool, default=True, help='shrink the embedding dictionary to corpus (open this if pre-trained embedding dictionary is too large, but disable this may yield better results on external corpus)')
-    parser.add_argument('--bichar', type=bool, default=False, help='use bichar or not')
+    parser.add_argument('--bichar', type=bool, default=True, help='use bichar or not')
     args = parser.parse_args()
 
     if args.gpu >= 0:
